979f705c88de8259377454ac0e18e7558f665288
Alexandre Rodrigues <novasystems.atendimento@hotmail.com>
2022-08-22 16:51:56 -0300

    update

--- public_html/includes/rodape.php
+++ public_html/includes/rodape.php
@@ -2294,7 +2294,7 @@
 		if ($usuario_nivel == 'ADMIN') {
 		 	?>
 		 	<script type="text/javascript">
-            var conn = new WebSocket('wss://epapodetarot.com.br/wss8787/NS');
+            var conn = new WebSocket('wss://epapodetarot.com.br/wss8787/NNN');
             function StatusTarologoOnline() {
               $('#onlineVerificaion').removeClass('offline');
               $('#onlineVerificaion').addClass('online');
