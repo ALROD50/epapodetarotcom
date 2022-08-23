8392161a9c3a8e05d38365939db20c160222ffbe
Alexandre Rodrigues <novasystems.atendimento@hotmail.com>
2022-08-23 18:26:33 -0300

    update

--- public_html/includes/rodape.php
+++ public_html/includes/rodape.php
@@ -2409,11 +2409,11 @@
 		<!-- Preloader -->
 		<script type="text/javascript">
 		    //<![CDATA[
-		    // $(window).on('load', function () { // makes sure the whole site is loaded 
-		    //     $('#preloader .inner').fadeOut(); // will first fade out the loading animation 
-		    //     $('#preloader').delay(200).fadeOut('slow'); // will fade out the white DIV that covers the website. 
-		    //     $('body').delay(200).css({'overflow': 'visible'});
-		    // })
+		    $(window).on('load', function () { // makes sure the whole site is loaded 
+		        $('#preloader .inner').fadeOut(); // will first fade out the loading animation 
+		        $('#preloader').delay(200).fadeOut('slow'); // will fade out the white DIV that covers the website. 
+		        $('body').delay(200).css({'overflow': 'visible'});
+		    })
 		    //]]>
 	    </script>
 
