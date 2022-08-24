b387969647a91a2f4ae775ad6c69a5947a6de76e
Alexandre Rodrigues <novasystems.atendimento@hotmail.com>
2022-08-24 17:35:04 -0300

    update

--- public_html/mensagens/enviadas.php
+++ public_html/mensagens/enviadas.php
@@ -110,15 +110,14 @@ if ($row > 0) {
 
 		<div class="row" style="margin: 5px;">
 
+			<div id="head2" style="font-size:11px;">
+				<?php echo $data; ?>
+			</div>			
 			<div id="head1" style="float:left;">
 				<?php echo 'De: <b>'.$nome_remetente.'</b><br/>'; ?>
 				<?php echo 'Para: <b>'.$nome_destinatario.'</b><br/>'; ?>
 				<?php echo 'Assunto: <b>'.$assunto.'</b>'; ?>
 			</div>
-			
-			<div id="head2" style="float:right; font-size:11px;">
-				<?php echo $data; ?>
-			</div>			
 
 		</div>
 
