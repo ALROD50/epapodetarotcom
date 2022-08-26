67ed50646ded94dd23f1ce32eb05bb5c5cf50862
Alexandre Rodrigues <novasystems.atendimento@hotmail.com>
2022-08-26 18:22:53 -0300

    update

--- public_html/site_pages/quem-somos.php
+++ public_html/site_pages/quem-somos.php
@@ -1,4 +1,8 @@
 <h1><i class="fas fa-ankh"></i> Quem Somos</h1>
 <hr>
 
-<p><b>É Papo de Tarot</b> é um sonho ultimamente, e movido por uma grande inspiração. Vontade de uma pessoa que já foi consulente e agora está em um papel de Taróloga. Nas minhas consultas como taróloga, sempre busquei não trazer para os meus atendimentos o que havia de ruim na minha experiência como consulente. Inicialmente, o trabalho começou com um aplicativo de comunicação, mas se tornou uma escala desorganizada, então decidimos oficializar tudo por um site. Gosto de estudar sobre símbolos e personalidades através do Tarot e planejo reunir meus conhecimentos para buscadores como eu.</p>
+<p><b>É Papo de Tarot</b> é um sonho ultimamente, e movido por uma grande inspiração. Vontade de uma pessoa que já foi consulente e agora está em um papel de Taróloga.</p>
+
+<p>Nas minhas consultas como taróloga, sempre busquei não trazer para os meus atendimentos o que havia de ruim na minha experiência como consulente. Inicialmente, o trabalho começou com um aplicativo de comunicação, mas se tornou uma escala desorganizada, então decidimos oficializar tudo por um site.</p>
+
+<p>Gosto de estudar sobre símbolos e personalidades através do Tarot e planejo reunir meus conhecimentos para buscadores como eu.</p>
