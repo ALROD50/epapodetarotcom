7fd7a54c02bb4ac660aebca939dd091ac8507a36
Alexandre Rodrigues <novasystems.atendimento@hotmail.com>
2022-08-26 20:04:52 -0300

    update

--- public_html/includes/topo.php
+++ public_html/includes/topo.php
@@ -333,8 +333,7 @@ if($row_onlinex!="offline" OR $row_onlinex!="") {
       // Sim - Estou Online! ---------------------------------------------------------
       // Volta o tarólogo automáticamente para a página de aguardando chamadas
       // Recarrega a página quando o tarólgo estiver fora da página minha-conta
-      echo $URLSUBCATEGORIA;
-      if ($URLSESSAO!='minha-conta') {
+      if ($URLSESSAO!='minha-conta' AND $URLSUBCATEGORIA!='resumo.php') {
         ?>
         <script>
           setTimeout(function(){
