d9c1f201056256f08c0f82e1f545602337d57e39
Alexandre Rodrigues <novasystems.atendimento@hotmail.com>
2022-08-26 18:47:42 -0300

    update

--- public_html/site_blog/site_categorias.php
+++ public_html/site_blog/site_categorias.php
@@ -1,7 +1,7 @@
 <div class="card card-body p-1 pb-4" style="background:#f5f5f5; font-size:19px;">
 
   <div class="text-center mt-2 my-0">
-    <h3><i class="fas fa-ankh"></i> Categorias</h3>
+    <h3>Categorias</h3>
     <hr>
   </div>
 
@@ -11,7 +11,7 @@
   while ($dadoss= $executa->fetch(PDO::FETCH_ASSOC)) { 
     $titulo=$dadoss['titulo'];
     $alias=$dadoss['alias'];
-    echo '<p class="ml-2 mb-0 py-0"><a href="blog/'.$alias.'" class="link-padraotres text-decoration-none">>> '.$titulo.'</a></p>';
+    echo '<p class="ml-2 mb-0 py-0"><a href="blog/'.$alias.'" class="link-padraotres text-decoration-none">- '.$titulo.'</a></p>';
   }
   ?>
 </div>
