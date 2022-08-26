e239a0180ec05d912de7e4a72acf07d7d4197b1c
Alexandre Rodrigues <novasystems.atendimento@hotmail.com>
2022-08-26 10:31:23 -0300

    update

--- public_html/site_rodape/rodape.php
+++ public_html/site_rodape/rodape.php
@@ -20,7 +20,7 @@ error_reporting(E_ALL); // Força o PHP a mostrar os erros.
 			<p><a href="quem-somos" class=""> Quem Somos</a></p>
 			<p><a href="lembrar-senha" class=""> Lembrar-Senha</a></p>
 			<p><a href="politica-de-privacidade-e-termos-de-uso" class=""> Termos de Uso</a></p>
-			<p><a href="https://forms.gle/nk7uQY3DSQ8o8PGc7" class=""> Trabalhe Conosco</a></p>
+			<p><a href="trabalhe-conosco" class=""> Trabalhe Conosco</a></p>
 			<p><a href="perguntas-frequentes" class=""> FAQ</a></p>
 			<p><a href="blog" class=""> Blog</a></p>
 		</div>
@@ -34,7 +34,7 @@ error_reporting(E_ALL); // Força o PHP a mostrar os erros.
 			<h2 class="vinho">Redes Sociais</h2>
 			<p><a href="https://www.facebook.com/epapodetarot/" target="_blank" title="Facebook É Papo de Tarot"><i class="fab fa-facebook-square fa-4x azul efeitodois"></i></a>
 			<a href="https://www.instagram.com/epapodetarot/" target="_blank" title="Instagram É Papo de Tarot"><i class="fab fa-instagram-square fa-4x instagram efeitodois"></i></a>
-			<!-- <a href="https://www.youtube.com/channel/UCAoHI95gpjGH3UiewFaiU-A?view_as=subscriber" target="_blank" title="Youtube É Papo de Tarot"><i class="fab fa-youtube-square fa-4x vermelho efeitodois"></i></a> -->
+			<a href="https://www.youtube.com/channel/UCAoHI95gpjGH3UiewFaiU-A?view_as=subscriber" target="_blank" title="Youtube É Papo de Tarot"><i class="fab fa-youtube-square fa-4x vermelho efeitodois"></i></a>
 			<a href="https://api.whatsapp.com/send?phone=5511994245002&amp;text=Olá É Papo de Tarot, pode me ajudar?" target="_blank"><img src="/images/whtzp.png" alt="" class="rounded" style="width:60px; top: -21px; position: relative;"></a></p>
 		</div>
 	</div>
