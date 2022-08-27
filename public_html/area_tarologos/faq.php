2412a8644eb7d0846c34f178bfff8ad8ae53458f
Alexandre Rodrigues <novasystems.atendimento@hotmail.com>
2022-08-27 12:58:19 -0300

    update

--- public_html/area_tarologos/faq.php
+++ public_html/area_tarologos/faq.php
@@ -1,7 +1,6 @@
 <h3 class="text-success mt-3"><i class="fas fa-book"></i> Faq - Informações Para Colaboradores</h3>
 <hr>
 
-<p><i class="fas fa-om"></i> Namastê!</p>
 <p>Essa sessão foi desenvolvida com a intenção de fornecer informações importantes sobre o uso do site, para manter nossa equipe sempre o mais atualizada possível, sinta-se a vontade para consultar essa página sempre que necessário.</p>
 
 <p><a href="minha-conta/?pg=area_tarologos/faqs/videochamadas.php"><i class="far fa-arrow-alt-circle-right"></i> <b>Novidade!</b> Instruções sobre o uso das Videochamadas.</a></p>
