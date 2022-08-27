265bb59b2bfe08301d54e18baf2258427ea50d77
Alexandre Rodrigues <novasystems.atendimento@hotmail.com>
2022-08-27 08:41:59 -0300

    update

--- public_html/includes/menu_site.php
+++ public_html/includes/menu_site.php
@@ -27,10 +27,10 @@
         </div>
       </li> -->
       <li class="nav-item">
-        <a class="nav-link menulink" href="comprar-consulta">COMPRAR</a>
+        <a class="nav-link menulink" href="perguntas-frequentes">COMO FUNCIONA?</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link menulink" href="perguntas-frequentes">COMO FUNCIONA?</a>
+        <a class="nav-link menulink" href="comprar-consulta">COMPRAR</a>
       </li>
       <li class="nav-item">
         <a class="nav-link menulink" href="registre-se">CADASTRE-SE</a>
