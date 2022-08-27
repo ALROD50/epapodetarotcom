5db0c12a872de81d84f8a13779a286bf883dfec8
Alexandre Rodrigues <novasystems.atendimento@hotmail.com>
2022-08-27 18:01:07 -0300

    update

--- public_html/area51/area51.php
+++ public_html/area51/area51.php
@@ -1,154 +1,56 @@
-<?php
-// Converte imagem em WEBP, a partir de um diretório, ou arquivo especifico
-$execulta="nao";
-$especifico="nao";
-$webpPARAjpg="nao";
-if ($execulta=="sim") {
-    echo "Entrou em execulta <br>";
-    if ($especifico=="sim") {
-        echo "Especifico sim <br>";
-        // Arquivo Especifico
-        $dir = '/home/epapodetarotcom/public_html/images/';
-        $name = 'whatsapp.webp';
-        $newName = 'whatsapp.webp';
-        $img = imagecreatefrompng($dir . $name);
-        imagepalettetotruecolor($img);
-        imagealphablending($img, true);
-        imagesavealpha($img, true);
-        imagewebp($img, $dir . $newName, 80); //qualidade
-        imagedestroy($img);
-        // imagecreatefrompng
-        // imagecreatefromgif
-        // imagecreatefrombmp
-    } else {
-        echo "Especifico não <br>";
-        /* Diretorio que deve ser lido */
-        $path = '/home/epapodetarotcom/public_html/images/metapropertyimg/';
-        /* Abre o diretório */
-        
-        // webp PARA jpg ?
-        $pasta=opendir($path);
-        
-        if ($webpPARAjpg=="sim") {
-            echo "webpPARAjpg sim<br>";
-            /* Loop para ler os arquivos do diretorio */
-            while ($arquivo = readdir($pasta)) {
-                echo "Loop para ler os arquivos do diretorio<br>";
-                /* Verificacao para exibir apenas os arquivos e nao os caminhos para diretorios superiores */
-                @$ext = strtolower(end(explode(".", $arquivo)));
-                if ($arquivo != '.' && $arquivo != '..' && $ext != 'zip' && $ext != 'php' && $ext != 'html' && $arquivo != 'error_log') {
-                    // Convertendo .WEBP para .JPG
-                    echo "Convertendo .WEBP para .JPG <br>";
-                    $dir = $path;
-                    $name = $arquivo;
-                    echo "path $path <br>";
-                    echo "arquivo $arquivo <br>";
-                    echo "ext $ext <br>";
-                    // Garante que a imagem é do tipo .WEBP
-                    if (!file_exists($dir.$name)) {
-                        echo "File does not exists <br>";
-                        // File does not exists
-                    }
-                    if ($ext == 'webp') {
-                        echo "convertendo <br>";
-                        @$newName = strtolower(reset(explode(".", $arquivo)));
-                        @$newName = $newName.'.png';
-                        $img = imagecreatefromwebp($dir . $name);
-                        imagejpeg($img, $dir . $newName, 80); //qualidade
-                        imagedestroy($img);
-                        echo "newName $newName <br>";
-                        // deleta a imagem anterior
-                        $filepath = $dir.$arquivo;
-                        echo "deleta a imagem anterior $filepath <br>";
-                        @unlink ($filepath);
-                    }
-                }
-            }
-        } else {  
-            echo "webpPARAjpg nao<br>";      
-            /* Loop para ler os arquivos do diretorio */
-            while ($arquivo = readdir($pasta)) {
-                /* Verificacao para exibir apenas os arquivos e nao os caminhos para diretorios superiores */
-                @$ext = strtolower(end(explode(".", $arquivo)));
-                if ($arquivo != '.' && $arquivo != '..' && $ext != 'zip' && $ext != 'php' && $ext != 'html' && $arquivo != 'error_log') {
-                    // Se for imagem
-                    if ($ext == 'jpg' OR $ext == 'jpeg' OR $ext == 'png' OR $ext == 'gif' OR $ext == 'bmp') {
-                        // Convertendo imagem em .WEBP
-                        if ($ext == 'jpg') {
-                            $dir = $path;
-                            $name = $arquivo;
-                            @$newName = strtolower(reset(explode(".", $arquivo)));
-                            @$newName = $newName.'.webp';
-                            $img = imagecreatefromjpeg($dir . $name);
-                            imagepalettetotruecolor($img);
-                            imagealphablending($img, true);
-                            imagesavealpha($img, true);
-                            imagewebp($img, $dir . $newName, 80); //qualidade
-                            imagedestroy($img);
-                            // deleta a imagem anterior
-                            $filepath = $dir.$arquivo;
-                            @unlink ($filepath);
-                        } elseif ($ext == 'jpeg') {
-                            $dir = $path;
-                            $name = $arquivo;
-                            @$newName = strtolower(reset(explode(".", $arquivo)));
-                            @$newName = $newName.'.webp';
-                            $img = imagecreatefromjpeg($dir . $name);
-                            imagepalettetotruecolor($img);
-                            imagealphablending($img, true);
-                            imagesavealpha($img, true);
-                            imagewebp($img, $dir . $newName, 80); //qualidade
-                            imagedestroy($img);
-                            // deleta a imagem anterior
-                            $filepath = $dir.$arquivo;
-                            @unlink ($filepath);
-                        } elseif ($ext == 'png') {
-                            $dir = $path;
-                            $name = $arquivo;
-                            @$newName = strtolower(reset(explode(".", $arquivo)));
-                            @$newName = $newName.'.webp';
-                            $img = imagecreatefrompng($dir . $name);
-                            imagepalettetotruecolor($img);
-                            imagealphablending($img, true);
-                            imagesavealpha($img, true);
-                            imagewebp($img, $dir . $newName, 80); //qualidade
-                            imagedestroy($img);
-                            // deleta a imagem anterior
-                            $filepath = $dir.$arquivo;
-                            @unlink ($filepath);
-                        } elseif ($ext == 'gif') {
-                            $dir = $path;
-                            $name = $arquivo;
-                            @$newName = strtolower(reset(explode(".", $arquivo)));
-                            @$newName = $newName.'.webp';
-                            $img = imagecreatefromgif($dir . $name);
-                            imagepalettetotruecolor($img);
-                            imagealphablending($img, true);
-                            imagesavealpha($img, true);
-                            imagewebp($img, $dir . $newName, 80); //qualidade
-                            imagedestroy($img);
-                            // deleta a imagem anterior
-                            $filepath = $dir.$arquivo;
-                            @unlink ($filepath);
-                        } elseif ($ext == 'bmp') {
-                            $dir = $path;
-                            $name = $arquivo;
-                            @$newName = strtolower(reset(explode(".", $arquivo)));
-                            @$newName = $newName.'.webp';
-                            $img = imagecreatefrombmp($dir . $name);
-                            imagepalettetotruecolor($img);
-                            imagealphablending($img, true);
-                            imagesavealpha($img, true);
-                            imagewebp($img, $dir . $newName, 80); //qualidade
-                            imagedestroy($img);
-                            // deleta a imagem anterior
-                            $filepath = $dir.$arquivo;
-                            @unlink ($filepath);
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
+<?php 
+$nomeDaSala = "23455";
+// $nomeDaSala = "789";
 ?>
+<script src='https://www.epapodetarot.com.br/area51/external_api.js'></script>
+<style>html, body, #meet { height: 800px;}</style>
+<script type="text/javascript">
+	window.onload = () => {
+		// const domain = 'meet.jit.si/someroomname#config.subject="CodigoNomeDaSala"';
+		const domain = 'meet.jit.si';
+		const options = {
+			roomName: '<?= $nomeDaSala; ?>',
+			lang: 'pt-BR',
+			configOverwrite: { 
+				startWithAudioMuted: false,
+				startWithVideoMuted: false,
+				disableDeepLinking: true,
+				rejoinPageEnabled: false,
+				prejoinPageEnabled: false,
+				requireDisplayName: false,
+				displayName: false,
+				disableProfile: false,
+				// meetingPasswordEnabled: false,
+			},
+			interfaceConfigOverwrite: { 
+				SHOW_WATERMARK_FOR_GUESTS: false,
+				SHOW_JITSI_WATERMARK: false,
+				SHOW_BRAND_WATERMARK: false,
+				HIDE_DEEP_LINKING_LOGO: true,
+				SHOW_DEEP_LINKING_IMAGE: false,
+				// SHOW_POWERED_BY: false,
+				SHOW_PROMOTIONAL_CLOSE_PAGE: false,
+				DEFAULT_REMOTE_DISPLAY_NAME: 'Tarólogo',
+				FILM_STRIP_MAX_HEIGHT: 2,
+				TOOLBAR_BUTTONS: [ 
+					'microphone', 'camera', 'videoquality', 'tileview'
+				],
+			},
+			parentNode: document.querySelector('#meet')
+		};
+		const api = new JitsiMeetExternalAPI(domain, options);
+		
+		// api.addEventListener('videoConferenceStart', () => {
+		// 	api.executeCommand('startRecording', {
+		// 		mode: 'file',
+		// 		dropboxToken: 'sl.BLrNk-dBr8fm3By0FcJvpLdMmiRh8ux_SRXeVSP6AXRcqf5FlnMuqYlZTh3jaujV_D6ZFvgmsWymHVT1wLMG0nLdvvPN2KAu2Me7VPdg_UUMBQQRdIQytHrRb8L-6i4E2mMkHuI',
+		// 	});
+		// 	api.executeCommand('stopRecording', 
+		// 		mode: string //recording mode to stop, `stream` or `file`
+		// 	);
+		// });
+	}
+</script>
+<a href="https://www.epapodetarot.com.br/minha-conta/?pg=area51/area51.php"><div id="logojitsi" style="background:#000000;width: 205px;height: 55px;position: relative;top: 74px; border-radius: 6px 6px 6px 6px;padding: 2px;"><img src="https://www.epapodetarot.com.br/images/Logo-Site.fw.png" style="width: 200px;height: 50px;"/></div></a>
+<div id="meet"></div>
+<!-- 'microphone', 'camera', 'closedcaptions', 'desktop', 'fullscreen', 'fodeviceselection', 'hangup', 'profile', 'recording', 'livestreaming', 'etherpad', 'sharedvideo', 'settings', 'raisehand', 'videoquality', 'filmstrip', 'feedback', 'stats', 'shortcuts', 'tileview' -->
