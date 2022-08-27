10265b2ce5bbcc5ab5e179c5f1f9e5bcb1f24915
Alexandre Rodrigues <novasystems.atendimento@hotmail.com>
2022-08-27 10:16:00 -0300

    update

--- public_html/site_pages/cadastrar.php
+++ public_html/site_pages/cadastrar.php
@@ -1,8 +1,4 @@
-<div class="card card-body pb-0 pt-2">
-  <h2 class="text-success"><i class="fas fa-om"></i> <span class="preto">Novo no site?</span></h2>
-</div>
 <hr>
-<h2><i class="fas fa-address-card"></i> Faça seu registro abaixo!</h2>
-<h3><i class="fas fa-mug-hot"></i> É rapidinho...</h3>
+<h2>Faça seu Registro</h2>
 <?php
 include "login/login_site.php";
