c5c98f4d8b4e4418117ed3b6fac470798d84bdbb
Alexandre Rodrigues <novasystems.atendimento@hotmail.com>
2022-08-27 10:14:59 -0300

    update

--- public_html/site_pages/perguntas-frequentes.php
+++ public_html/site_pages/perguntas-frequentes.php
@@ -1,28 +1,24 @@
-<h1><i class="fas fa-question-circle"></i> Perguntas Frequentes</h1>
+<h1>Perguntas Frequentes</h1>
 <hr>
 
-<h3 class="vermelho"><i class="fas fa-folder-minus"></i> As consultas são gratuitas?</h3>
+<h3 class="vinho">As consultas são gratuitas?</h3>
 
 <p>Não. Nossas consultas são tarifadas, porém temos um valor bem acessível e variados meios de pagamento para ajudar a todos.<br>Para consultar-se via chat é cobrado o valor do minuto que custa R$ 1,50. Temos vários pacotes e promoções feitas especialmente para atender você.</p>
 
-<h3 class="vermelho"><i class="fas fa-folder-minus"></i> Posso consultar por Telefone?</h3>
+<h3 class="vinho">Posso consultar por Telefone?</h3>
 
 <p>Você pode fazer uma consulta via Vídeo Chamada com audio e video ao vivo dentro do site, consulte os tarólogos disponíveis. Tanto o tarólogo como o cliente não tem obrigação de mostrar o rosto. Se a consulta for do tipo videochamada é possível deixar a camera desligada, ou não mostrar o rosto.</p>
 
-<h3 class="vermelho"><i class="fas fa-folder-minus"></i> Como eu faço para me consultar?</h3>
+<h3 class="vinho">Como eu faço para me consultar?</h3>
 
 <p>É muito fácil para se consultar,</p>
 
 <p style="padding-left: 20px;">1 - Você precisa primeiramente&nbsp;<a href="registre-se">Cadastrar-se</a>&nbsp;em nosso site.<br>2 - Em seguida, você deve&nbsp;<a href="comprar-consulta">Comprar seus Créditos</a>&nbsp;de acordo com o pacote de minutos desejado.<br>3 - E finalmente, escolha&nbsp;um&nbsp;<a href="tarologos">Especialista</a>&nbsp;para iniciar sua consulta online.</p>
 
-<h3 class="azul"><i class="fas fa-money-bill-alt"></i> Financeiro</h3>
-
-<h3 class="vermelho"><i class="fas fa-folder-minus"></i> Quais formas de Pagamento são aceitas?</h3>
+<h3 class="vinho">Quais formas de Pagamento são aceitas?</h3>
 
 <p>Trabalhamos com Pix, Boletos e Cartão de Crédito, aceitamos várias bandeiras.</p>
 
-<h3 class="vermelho"><i class="fas fa-folder-minus"></i> É seguro realizar um pagamento?</h3>
+<h3 class="vinho">É seguro realizar um pagamento?</h3>
 
 <p>Visando à segurança dos seus dados, nosso site dispõe de um moderno sistema criptografado, no qual todas as informações do cadastro e formas de pagamento são codificadas, impedindo a cópia e/ou leitura por terceiros. Há uma proteção total para os seus dados. Nem nós temos acesso aos seus dados de pagamento!</p>
-
-<p>Link para comprar minutos:&nbsp;<a href="comprar-consulta">Comprar seus Créditos.<i class="fas fa-rocket"></i></a>
