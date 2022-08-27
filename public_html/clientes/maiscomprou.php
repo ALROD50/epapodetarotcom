456c2b28c7e557b9415a9c505b2bd383618755a2
Alexandre Rodrigues <novasystems.atendimento@hotmail.com>
2022-08-27 11:11:35 -0300

    update

--- public_html/clientes/maiscomprou.php
+++ public_html/clientes/maiscomprou.php
@@ -1,8 +1,9 @@
 <div class="row">
 	<div class="col-md-12">
 		<div class="well muted">
-			<h3 class="text-success">Clientes Que Mais Compraram no É Papo de Tarot</h3>
+			<h3 class="text-success">Clientes Que Mais Compraram no Templo Qsara</h3>
 			<hr>
+			<p>Abra um chamado no departamento de Atualizações da Nova Systems para saber como adiquerir este recurso.</p>
 			<?php
 			// Selecione os clientes com mais compras
 			$resultxx = $pdo->query("SELECT count(id_nome_cliente)Qtde_Registros, id_nome_cliente from controle group by id_nome_cliente order by Qtde_Registros DESC LIMIT 100");
@@ -21,8 +22,8 @@
 					$telefone =  remover_caracter($telefone); 
 					$telefone =  preg_replace("/_/", "", $telefone);
 					?>
-					<p>Compras: <b><?php echo $Qtde_Registros; ?></b> | <?php echo "<a href='minha-conta/?pg=clientes/view.php&id=$id_cliente' data-toggle='tooltip' title='$nome'>$nome</a>"; ?> | <a href='<?php echo "https://api.whatsapp.com/send?phone=55$telefone&text=Oi $nome, tudo bem? Gostaria de fazer uma consultinha hoje no É Papo de Tarot e ver como esta o Trabalho, Saúde, Família, Amor etc...?"; ?>' target="_Blank" class="linkClientesAdmin"><i class="fab fa-whatsapp"></i> Mandar Mensagem</a></p>
-					<hr>
+					<p class="d-none">Compras: <b><?php echo $Qtde_Registros; ?></b> | <?php echo "<a href='minha-conta/?pg=clientes/view.php&id=$id_cliente' data-toggle='tooltip' title='$nome'>$nome</a>"; ?> | <a href='<?php echo "https://api.whatsapp.com/send?phone=55$telefone&text=Oi $nome, tudo bem?"; ?>' target="_Blank" class="linkClientesAdmin"><i class="fab fa-whatsapp"></i> Mandar Mensagem</a></p>
+					<hr class="d-none">
 					<?php
 				}
 			}
