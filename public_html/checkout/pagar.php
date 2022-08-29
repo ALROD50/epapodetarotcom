005e7f3ae1ec420e1ec65d891c9f0295ac5f1014
Alexandre Rodrigues <novasystems.atendimento@hotmail.com>
2022-08-29 14:38:35 -0300

    update

--- public_html/checkout/pagar.php
+++ public_html/checkout/pagar.php
@@ -363,7 +363,7 @@ require 'processa.php';
                     	<a href="#tabBoleto" data-toggle="tab" class="flex-sm-fill text-sm-center nav-link shadow" id="tabBoleto-tab" role="tab" aria-controls="tabBoleto" aria-selected="false"><i class="fas fa-barcode"></i> Boleto</a>
                     </li>
                    
-                    <li class="nav-item" role="presentation">
+                    <li class="nav-item d-none" role="presentation">
                     	<a href="#tabDeposito" data-toggle="tab" class="flex-sm-fill text-sm-center nav-link shadow" id="tabDeposito-tab" role="tab" aria-controls="tabDeposito" aria-selected="false"><i class="fas fa-university"></i> Transferência</a>
                     </li>
                 </ul>
@@ -917,7 +917,7 @@ require 'processa.php';
                     </div>
 
                     <!-- Depósito -->
-                    <div class="tab-pane fade show" id="tabDeposito" role="tabpanel" aria-labelledby="tabDeposito-tab">
+                    <div class="tab-pane fade show d-none" id="tabDeposito" role="tabpanel" aria-labelledby="tabDeposito-tab">
 
                         <h2 class="mb-4">Transferência</h2>
 
