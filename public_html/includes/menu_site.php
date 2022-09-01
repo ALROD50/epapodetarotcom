8dba8786b4ae920aeda76f9aa0a3084d6df8fd6c
Alexandre Rodrigues <novasystems.atendimento@hotmail.com>
2022-09-01 15:51:18 -0300

    update

--- public_html/includes/menu_site.php
+++ public_html/includes/menu_site.php
@@ -6,73 +6,75 @@
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
-    <!-- <ul id="submenuce" class="nav justify-content-center"> -->
-      <li class="nav-item active">
-        <a class="nav-link menulink" href="home">HOME<span class="sr-only"></span></a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle menulink" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          CONSULTA
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="registre-se">CADASTRE-SE</a>
-          <a class="dropdown-item" href="comprar-consulta">COMPRAR</a>
-          <a class="dropdown-item" href="tarologos">TARÓLOGOS</a>
-        </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link menulink" href="perguntas-frequentes">COMO FUNCIONA?</a>
-      </li>
-      <!-- <li class="nav-item">
-        <a class="nav-link menulink" href="registre-se">CADASTRE-SE</a>
-      </li> -->
-      <!-- <li class="nav-item">
-        <a class="nav-link menulink" href="comprar-consulta">COMPRAR</a>
-      </li> -->
-      <!-- <li class="nav-item">
-        <a class="nav-link menulink" href="tarologos">TARÓLOGOS</a>
-      </li> -->
-      <!-- <li class="nav-item">
-        <a class="nav-link menulink" href="loja">LOJA</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link menulink" href="loja-item/mapa-astral/mapa-astral">MAPA ASTRAL</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link menulink" href="loja-item/mapa-numerologico/mapa-numerologico">NUMEROLOGIA</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link menulink" href="loja-item/reiki/reiki">REIKI</a>
-      </li> -->
-      <li class="nav-item">
-        <a class="nav-link menulink" href="blog">BLOG</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle menulink" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          OUTROS ASSUNTOS
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="faleconosco">FALE CONOSCO</a>
-          <a class="dropdown-item" href="https://dottovip.com.br/agendamentosdeconsultas/">AGENDAMENTO</a>
-        </div>
-      </li>
-      <!-- <li class="nav-item">
-        <a class="nav-link menulink" href="https://dottovip.com.br/agendamentosdeconsultas/">AGENDAMENTO</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link menulink" href="faleconosco">FALE CONOSCO</a>
-      </li> -->
-      <!-- <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle menulink" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          CATEGORIAS
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">RUNAS</a>
-          <a class="dropdown-item" href="#">HORÓSCOPO</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">ASTROLOGIA</a>
-        </div>
-      </li> -->
+      <div class="row justify-content-center">
+        <!-- <ul id="submenuce" class="nav justify-content-center"> -->
+        <li class="nav-item active">
+          <a class="nav-link menulink" href="home">HOME<span class="sr-only"></span></a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle menulink" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            CONSULTA
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="registre-se">CADASTRE-SE</a>
+            <a class="dropdown-item" href="comprar-consulta">COMPRAR</a>
+            <a class="dropdown-item" href="tarologos">TARÓLOGOS</a>
+          </div>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link menulink" href="perguntas-frequentes">COMO FUNCIONA?</a>
+        </li>
+        <!-- <li class="nav-item">
+          <a class="nav-link menulink" href="registre-se">CADASTRE-SE</a>
+        </li> -->
+        <!-- <li class="nav-item">
+          <a class="nav-link menulink" href="comprar-consulta">COMPRAR</a>
+        </li> -->
+        <!-- <li class="nav-item">
+          <a class="nav-link menulink" href="tarologos">TARÓLOGOS</a>
+        </li> -->
+        <!-- <li class="nav-item">
+          <a class="nav-link menulink" href="loja">LOJA</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link menulink" href="loja-item/mapa-astral/mapa-astral">MAPA ASTRAL</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link menulink" href="loja-item/mapa-numerologico/mapa-numerologico">NUMEROLOGIA</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link menulink" href="loja-item/reiki/reiki">REIKI</a>
+        </li> -->
+        <li class="nav-item">
+          <a class="nav-link menulink" href="blog">BLOG</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle menulink" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            OUTROS ASSUNTOS
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="faleconosco">FALE CONOSCO</a>
+            <a class="dropdown-item" href="https://dottovip.com.br/agendamentosdeconsultas/">AGENDAMENTO</a>
+          </div>
+        </li>
+        <!-- <li class="nav-item">
+          <a class="nav-link menulink" href="https://dottovip.com.br/agendamentosdeconsultas/">AGENDAMENTO</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link menulink" href="faleconosco">FALE CONOSCO</a>
+        </li> -->
+        <!-- <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle menulink" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            CATEGORIAS
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#">RUNAS</a>
+            <a class="dropdown-item" href="#">HORÓSCOPO</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">ASTROLOGIA</a>
+          </div>
+        </li> -->
+      </div>
     </ul>
     <form class="form-inline my-2 my-lg-0 d-none">
         <input class="form-control mr-sm-2 d-none d-xl-block col-xl-12" type="search" placeholder="Pesquisar" aria-label="Pesquisar">
