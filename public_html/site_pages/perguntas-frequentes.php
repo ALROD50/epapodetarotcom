9abcfb29113b39fe96e50d47ee0a145873731262
Alexandre Rodrigues <novasystems.atendimento@hotmail.com>
2022-09-05 12:20:32 -0300

    update

--- public_html/site_pages/perguntas-frequentes.php
+++ public_html/site_pages/perguntas-frequentes.php
@@ -13,7 +13,7 @@
 
 <p>É muito fácil!</p>
 
-<p style="padding-left: 20px;">1 - Primeiramente, você precisa<a href="registre-se"><b>Cadastrar-se</b></a>&nbsp;em nosso site.<br>2 - Em seguida, você deve&nbsp;<a href="comprar-consulta"><b>Comprar seus Créditos</b></a>&nbsp;de acordo com o pacote de minutos desejado.<br>3 - E, finalmente escolher&nbsp;um&nbsp;<a href="tarologos"><b>Especialista</b></a>&nbsp;para iniciar sua consulta online.</p>
+<p style="padding-left: 20px;">1 - Primeiramente, você precisa <a href="registre-se"><b>Cadastrar-se</b></a>&nbsp;em nosso site.<br>2 - Em seguida, você deve&nbsp;<a href="comprar-consulta"><b>Comprar seus Créditos</b></a>&nbsp;de acordo com o pacote de minutos desejado.<br>3 - E, finalmente escolher&nbsp;um&nbsp;<a href="tarologos"><b>Especialista</b></a>&nbsp;para iniciar sua consulta online.</p>
 
 <h3 class="vinho">Quais formas de Pagamento são aceitas?</h3>
 
