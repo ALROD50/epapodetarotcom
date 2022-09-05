83ddba874906c8a6dbf10cb9d1fdea14833edd36
Alexandre Rodrigues <novasystems.atendimento@hotmail.com>
2022-09-05 15:45:51 -0300

    update

--- public_html/site_tarologos/tarologo_perfil.php
+++ public_html/site_tarologos/tarologo_perfil.php
@@ -44,65 +44,65 @@ if ($row > 0){
     </figure>
   </div>
 
-  <div class="card p-2 mt-2">
-        <p><u><i class="fas fa-arrow-right"></i> Atendimentos Disponíveis:</u></p>
-        <figcaption class="figure-caption">
+  <div class="row card p-2 mt-5 mb-1">
+    <p><u><i class="fas fa-arrow-right"></i> Atendimentos Disponíveis:</u></p>
+    <figcaption class="figure-caption">
 
-          <!-- Botões -->
-          <?php
-          if ($row_online == "offline" OR $row_online == ""){
-            ?>
-            <!-- Botão Avise-me Quando Disponível -->
-            <form name="aviseme_envia" id="aviseme_envia" method="post" action="aviseme-quando-disponivel" style="margin-top:5px;">
-              <input type="hidden" name="id_tarologo" value="<?php echo $id_tarologo; ?>" />
-              <button type="submit" name="envia" class="btn corpadrao2 btn-lg" alt="Avise-me Quando Disponível" title="Avise-me Quando Disponível"><i class="fas fa-clock"></i> Avise-me Quando Disponível</button>
-              <p class="mt-3">Este tarólogo não está online no momento, clique no botão acima <b>Avise-me Quando Disponível</b> para ser avisado quando este tarólog voltar online. Desta forma quando tarólogo voltar ao trabalho você será avisado por e-mail e poderá fazer sua consulta com ele.</p>
-            </form>
-            <!-- Botão Consulta por Email -->
-            <form name="consultarporemail" id="consultarporemail" method="post" action="consultarporemail" style="float:left; margin-right:8px;">
-              <input type="hidden" name="id_tarologo" value="<?php echo $id_tarologo; ?>" />
-              <button type="submit" name="envia" class="btn corpadrao2 btn-lg" alt="Consultar por E-mail" title="Consultar por E-mail"><i class="fas fa-envelope"></i> Consultar por E-mail</button>
-            </form>
-            <?php
-            
-          } elseif ($row_online == "online") {
-            
-            ?>
-            <form name="Consultar" id="Consultar<?php echo @$id; ?>" method="post" action="consultar" style="margin-top:5px;">
-              <input type="hidden" name="id_tarologo" value="<?php echo @$id_tarologo; ?>" />
-              <input type="hidden" name="id_cliente" value="<?php echo @$usuario_id; ?>" />
-              <input type="hidden" name="videochat" value="NAO" />
-              <button type="submit" name="envia" class="btn btn-lg btn-success"><i class="fas fa-comments"></i> CONSULTAR AO VIVO POR: <u>CHAT</u></button>
-            </form>
-            <?php
-            if ($videochamada=="SIM") {
-              ?>
-              <form name="Consultar" id="Consultar<?php echo @$id; ?>" method="post" action="consultar" style="margin-top:5px;">
-                <input type="hidden" name="id_tarologo" value="<?php echo @$id_tarologo; ?>" />
-                <input type="hidden" name="id_cliente" value="<?php echo @$usuario_id; ?>" />
-                <input type="hidden" name="videochat" value="SIM" />
-                <button type="submit" name="envia" class="btn btn-lg btn-warning"><i class="fas fa-video"></i> CONSULTAR AO VIVO POR: <u>VIDEO</u></button>
-              </form>
-              <?php
-            }
-            ?>
-            <!-- Botão Consultar Agora -->
-            <?php
-            
-          } elseif ($row_online == "ocupado") {
-
-            ?>
-            <!-- Botão Ocupado -->
-            <div class="mb-2">
-              <button type="submit" class="btn btn-danger btn-lg" disabled><i class="fas fa-hourglass-start"></i> Estou Ocupado</button>
-              <p>Este tarólogo esta ocupado em um atendimento neste momento, volte em alguns minutinhos para tentar novamente.</p>
-            </div>
-            <?php 
-
-          }
+      <!-- Botões -->
+      <?php
+      if ($row_online == "offline" OR $row_online == ""){
+        ?>
+        <!-- Botão Avise-me Quando Disponível -->
+        <form name="aviseme_envia" id="aviseme_envia" method="post" action="aviseme-quando-disponivel" style="margin-top:5px;">
+          <input type="hidden" name="id_tarologo" value="<?php echo $id_tarologo; ?>" />
+          <button type="submit" name="envia" class="btn corpadrao2 btn-lg" alt="Avise-me Quando Disponível" title="Avise-me Quando Disponível"><i class="fas fa-clock"></i> Avise-me Quando Disponível</button>
+          <p class="mt-3">Este tarólogo não está online no momento, clique no botão acima <b>Avise-me Quando Disponível</b> para ser avisado quando este tarólog voltar online. Desta forma quando tarólogo voltar ao trabalho você será avisado por e-mail e poderá fazer sua consulta com ele.</p>
+        </form>
+        <!-- Botão Consulta por Email -->
+        <form name="consultarporemail" id="consultarporemail" method="post" action="consultarporemail" style="float:left; margin-right:8px;">
+          <input type="hidden" name="id_tarologo" value="<?php echo $id_tarologo; ?>" />
+          <button type="submit" name="envia" class="btn corpadrao2 btn-lg" alt="Consultar por E-mail" title="Consultar por E-mail"><i class="fas fa-envelope"></i> Consultar por E-mail</button>
+        </form>
+        <?php
+        
+      } elseif ($row_online == "online") {
+        
+        ?>
+        <form name="Consultar" id="Consultar<?php echo @$id; ?>" method="post" action="consultar" style="margin-top:5px;">
+          <input type="hidden" name="id_tarologo" value="<?php echo @$id_tarologo; ?>" />
+          <input type="hidden" name="id_cliente" value="<?php echo @$usuario_id; ?>" />
+          <input type="hidden" name="videochat" value="NAO" />
+          <button type="submit" name="envia" class="btn btn-lg btn-success"><i class="fas fa-comments"></i> CONSULTAR AO VIVO POR: <u>CHAT</u></button>
+        </form>
+        <?php
+        if ($videochamada=="SIM") {
           ?>
-        </figcaption>
-      </div>
+          <form name="Consultar" id="Consultar<?php echo @$id; ?>" method="post" action="consultar" style="margin-top:5px;">
+            <input type="hidden" name="id_tarologo" value="<?php echo @$id_tarologo; ?>" />
+            <input type="hidden" name="id_cliente" value="<?php echo @$usuario_id; ?>" />
+            <input type="hidden" name="videochat" value="SIM" />
+            <button type="submit" name="envia" class="btn btn-lg btn-warning"><i class="fas fa-video"></i> CONSULTAR AO VIVO POR: <u>VIDEO</u></button>
+          </form>
+          <?php
+        }
+        ?>
+        <!-- Botão Consultar Agora -->
+        <?php
+        
+      } elseif ($row_online == "ocupado") {
+
+        ?>
+        <!-- Botão Ocupado -->
+        <div class="mb-2">
+          <button type="submit" class="btn btn-danger btn-lg" disabled><i class="fas fa-hourglass-start"></i> Estou Ocupado</button>
+          <p>Este tarólogo esta ocupado em um atendimento neste momento, volte em alguns minutinhos para tentar novamente.</p>
+        </div>
+        <?php 
+
+      }
+      ?>
+    </figcaption>
+  </div>
 
   <?php echo $infos; ?>
 
