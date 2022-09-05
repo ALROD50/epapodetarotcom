0a49c3c8e1be384d8087416f08656e5a1a669240
Alexandre Rodrigues <novasystems.atendimento@hotmail.com>
2022-09-05 15:14:20 -0300

    update

--- public_html/includes/topo.php
+++ public_html/includes/topo.php
@@ -6,7 +6,7 @@ $rowQuantidade = $sql_produto->rowCount();
 <div id="header" class="row">
 
   <!-- somente pc -->
-  <div class="row justify-content-center">
+  <div class="row col-md-12 justify-content-center">
     <div id="logo" class="d-xl-block d-lg-block d-none">
       <a href="https://www.epapodetarot.com.br/home" title="Site É Papo de Tarot" class="p-0 m-0">
         <center><img id="sizelogo" src="images/Logo-Site.fw.png" alt="Site É Papo de Tarot" title="Site É Papo de Tarot"></center>
