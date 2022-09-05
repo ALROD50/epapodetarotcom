320b5fc12b2b8d7e71446256a0f8a2e0f49e93b7
Alexandre Rodrigues <novasystems.atendimento@hotmail.com>
2022-09-05 12:02:40 -0300

    update

--- public_html/site_pages/quem-somos.php
+++ public_html/site_pages/quem-somos.php
@@ -1,8 +1,28 @@
 <h1>Quem Somos</h1>
 <hr>
 
-<p><b>É Papo de Tarot</b> é um sonho ultimamente, e movido por uma grande inspiração. Vontade de uma pessoa que já foi consulente e agora está em um papel de Taróloga.</p>
+<p>Prazer, sou Monique. Tudo bem?</p>
 
-<p>Nas minhas consultas como taróloga, sempre busquei não trazer para os meus atendimentos o que havia de ruim na minha experiência como consulente. Inicialmente, o trabalho começou com um aplicativo de comunicação, mas se tornou uma escala desorganizada, então decidimos oficializar tudo por um site.</p>
+<p>Sou a idealizadora do portal <b>É Papo de Tarot</b>. E gostaria de me apresentar para você. Assim a gente se conhece um pouquinho mais.</p>
 
-<p>Gosto de estudar sobre símbolos e personalidades através do Tarot e planejo reunir meus conhecimentos para buscadores como eu.</p>
+<p>Para começar, sou apaixonada por doces e por animais. Guarde essa informação porque você entenderá mais adiante =)</p>
+
+<p>Sou uma mulher corajosa que adora se arriscar o suficiente para conquistar seus sonhos. Como prova disso, fui morar sozinha aos meus 18 anos, assim, na cara e na coragem. E até que me dei bem no primeiro momento, pois encontrei uma vila na qual moravam 3 senhoras e elas não permitiam mulheres solteiras. Esse era um dos critérios. Como eu buscava por um lugar seguro, aceitei os critérios e fui morar lá. Um lugar tão tranquilo e de tanta paz que até parecia aquele filme do Chico Xavier, Nosso Lar.</p>
+
+<p>Depois de um tempinho lá já, comecei a reparar que uma das vizinhas recebia muita visita. O que você acha que passou pela minha cabeça? Ela vende doces! Lembra que sou apaixonada por doces? Então, lá fui eu encomendar um bolo. Chegando lá, veio a surpresa: ela me disse que não vendia bolos, que o fluxo de pessoas em sua casa era porque ela lia cartas. E ela ainda soltou essa: você viverá disso um dia, sabia?</p>
+
+<p>Eu ri! Não sei se porque realmente achei engraçado ou se estava rindo de nervoso. Até hoje, na real, não sei muito bem. O que importa é que naquele dia ganhei minha primeira consulta com uma taróloga e foi um momento bastante especial, porque ao fim da consulta, ela me pediu um momento e pegou um baralho comum, bem novinho, e escreveu os significados à mão. Significou muito para mim, pois guardo o baralho comigo até hoje.</p>
+
+<p>De lá pra cá, estudei sozinha, e muito! Adquiri alguns conhecimentos aqui, outros ali, mas nunca acreditei completamente que essa seria minha profissão um dia. Mesmo assim, a paixão pelas cartas só foi aumentando, mais oportunidades de aprendizado foram chegando e nos momentos mais difíceis da minha vida, todas as pessoas que me estenderam a mão de alguma forma, tinham contato ou conhecimentos de oráculos.</p>
+
+<p>Entendi que a minha vida sempre esteve conectada com esse universo das cartas. E como dizem que nada é por acaso, resolvi me jogar de cabeça nos estudos, realizei diversos cursos e hoje, eu sou taróloga! Além disso, ministro workshops de borra de café, simbologia e as minhas queridinhas cartas da corte no tarot.</p>
+
+<p>E como surgiu a ideia desse portal?</p>
+
+<p>Inicialmente, realizei vários atendimentos via WhatsApp, através de perguntas avulsas, para as quais as pessoas praticamente me pagavam um valor quase que simbólico por cada pergunta. Só que tem um porém: eu nunca consegui me ater exclusivamente àquela pergunta, sabe? Porque tarot não é adivinhação, tarot é bússola, aconselhamento e direcionamento … e eu sentia que precisava de mais tempo para me conectar com o consulente.</p>
+
+<p>Os minutos acabaram virando horas, principalmente porque se tornam necessários os esclarecimentos e direcionamentos. Mesmo atropelando toda minha agenda, eu me recusava deixar o consulente sair da consulta sem todos os esclarecimentos. Por isso, eu decidi criar este portal, como todo carinho, para que você possa comprar minutos da sua vontade para nossos atendimentos, e realizar todas as perguntas que desejar e precisar.</p>
+
+<p>E para fazer parte dessa empreitada incrível que é ajudar e orientar pessoas através do tarot, chamei algumas tarólogas de confiança para me apoiar e se colocarem à sua disposição!</p>
+
+<p>Bem-vindo e bem-vinda ao É Papo de Tarot!</p>
