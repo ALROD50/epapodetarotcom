6c1e4a6ff56b3747b7055ce96786b27146daec65
Alexandre Rodrigues <novasystems.atendimento@hotmail.com>
2022-09-05 12:20:07 -0300

    update

--- public_html/site_pages/perguntas-frequentes.php
+++ public_html/site_pages/perguntas-frequentes.php
@@ -7,17 +7,17 @@
 
 <h3 class="vinho">Posso consultar por Telefone?</h3>
 
-<p>Você pode fazer uma consulta via Vídeo Chamada com audio e video ao vivo dentro do site, consulte os tarólogos disponíveis. Tanto o tarólogo como o cliente não tem obrigação de mostrar o rosto. Se a consulta for do tipo videochamada é possível deixar a camera desligada, ou não mostrar o rosto.</p>
+<p>Você pode fazer uma consulta via video chamada, com áudio e vídeo ao vivo dentro do site. Consulte os tarólogos disponíveis. Tanto o tarólogo como o consulente não têm obrigação de mostrar o rosto. Se a consulta for do tipo videochamada, é possível deixar a câmera desligada ou não mostrar o rosto.</p>
 
 <h3 class="vinho">Como eu faço para me consultar?</h3>
 
-<p>É muito fácil para se consultar,</p>
+<p>É muito fácil!</p>
 
-<p style="padding-left: 20px;">1 - Você precisa primeiramente&nbsp;<a href="registre-se"><b>Cadastrar-se</b></a>&nbsp;em nosso site.<br>2 - Em seguida, você deve&nbsp;<a href="comprar-consulta"><b>Comprar seus Créditos</b></a>&nbsp;de acordo com o pacote de minutos desejado.<br>3 - E finalmente, escolha&nbsp;um&nbsp;<a href="tarologos"><b>Especialista</b></a>&nbsp;para iniciar sua consulta online.</p>
+<p style="padding-left: 20px;">1 - Primeiramente, você precisa<a href="registre-se"><b>Cadastrar-se</b></a>&nbsp;em nosso site.<br>2 - Em seguida, você deve&nbsp;<a href="comprar-consulta"><b>Comprar seus Créditos</b></a>&nbsp;de acordo com o pacote de minutos desejado.<br>3 - E, finalmente escolher&nbsp;um&nbsp;<a href="tarologos"><b>Especialista</b></a>&nbsp;para iniciar sua consulta online.</p>
 
 <h3 class="vinho">Quais formas de Pagamento são aceitas?</h3>
 
-<p>Trabalhamos com Pix e Cartão de Crédito, aceitamos várias bandeiras.</p>
+<p>Trabalhamos com Pix e cartão de crédito. Aceitamos várias bandeiras.</p>
 
 <h3 class="vinho">É seguro realizar um pagamento?</h3>
 
