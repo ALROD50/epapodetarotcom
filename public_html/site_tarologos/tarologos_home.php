fcde1870426b7427a24746bb57f68e87a487afe7
Alexandre Rodrigues <novasystems.atendimento@hotmail.com>
2022-09-09 10:50:10 -0300

    update

--- public_html/site_tarologos/tarologos_home.php
+++ public_html/site_tarologos/tarologos_home.php
@@ -318,7 +318,7 @@ $pdo->query("DELETE FROM chamada_consulta WHERE id_cliente='$usuario_id'");
     $infos2=$mostrar2['infos'];
     $descricaocurta=$mostrar2['infos2'];
     $descricaocurta = strip_tags($descricaocurta);
-    $descricaocurta = strip_tags(limita_caracteres($descricaocurta, 250, true));
+    $descricaocurta = strip_tags(limita_caracteres($descricaocurta, 200, true));
     $logo2=$mostrar2['logo'];
     $videochamada=$mostrar2['videochamada'];
     // Verificando se tarólogo esta online.
