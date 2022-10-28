5f41d48575504397e4f25b81618ae59a7106dd25
Alexandre Rodrigues <novasystems.atendimento@hotmail.com>
2022-10-28 08:31:17 -0300

    update

--- public_html/area51/area51.php
+++ public_html/area51/area51.php
@@ -1,6 +1,6 @@
 <?php 
 $regex = "/^(?:[\p{Lu}&&[\p{IsLatin}]])(?:(?:')?(?:[\p{Ll}&&[\p{IsLatin}]]))+(?:\-(?:[\p{Lu}&&[\p{IsLatin}]])(?:(?:')?(?:[\p{Ll}&&[\p{IsLatin}]]))+)*(?: (?:(?:e|y|de(?:(?: la| las| lo| los))?|do|dos|da|das|del|van|von|bin|le) )?(?:(?:(?:d'|D'|O'|Mc|Mac|al\-))?(?:[\p{Lu}&&[\p{IsLatin}]])(?:(?:')?(?:[\p{Ll}&&[\p{IsLatin}]]))+|(?:[\p{Lu}&&[\p{IsLatin}]])(?:(?:')?(?:[\p{Ll}&&[\p{IsLatin}]]))+(?:\-(?:[\p{Lu}&&[\p{IsLatin}]])(?:(?:')?(?:[\p{Ll}&&[\p{IsLatin}]]))+)*))+(?: (?:Jr\.|II|III|IV))?$/";
-$nome = "Ale";
+$nome = "Scarlett O'Hara";
 $resultado = preg_match($regex, $nome);
 
 if ($resultado) { 
