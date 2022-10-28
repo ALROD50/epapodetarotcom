f231b91bc0cb13377863c31c3a80ef8b884c37a2
Alexandre Rodrigues <novasystems.atendimento@hotmail.com>
2022-10-28 11:14:57 -0300

    update

--- public_html/area51/area51.php
+++ public_html/area51/area51.php
@@ -1,16 +1,13 @@
 <?php 
+// Data de nascimento
 $nascimento = "07-08-1986";
-
-
-// separando yyyy, mm, ddd
+// Separando yyyy, mm, ddd
 list($ano, $mes, $dia) = explode('-', $nascimento);
-
-// data atual
+// Data atual
 $hoje = mktime(0, 0, 0, date('m'), date('d'), date('Y'));
 // Descobre a unix timestamp da data de nascimento do fulano
 $nascimento = mktime( 0, 0, 0, $mes, $dia, $ano);
-
-
+// Idade 
 $idade = floor((((($hoje - $nascimento) / 60) / 60) / 24) / 365.25);
 echo $idade;
 
