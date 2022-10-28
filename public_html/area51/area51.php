a4240c4cf43d69e297e81d41e8d3710f11fa6998
Alexandre Rodrigues <novasystems.atendimento@hotmail.com>
2022-10-28 11:11:08 -0300

    update

--- public_html/area51/area51.php
+++ public_html/area51/area51.php
@@ -2,7 +2,7 @@
 $nascimento = date("Y-m-d", strtotime("07-08-1986"));
 $hoje = date("Y-m-d");
 $idade = floor((((($hoje - $nascimento) / 60) / 60) / 24) / 365.25);
-echo $idade;
+echo $nascimento;
 
 
 ?>
