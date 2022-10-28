f68c0cd92f5e69ea1782c6969b6d87d4251dbe31
Alexandre Rodrigues <novasystems.atendimento@hotmail.com>
2022-10-28 08:37:18 -0300

    update

--- public_html/area51/area51.php
+++ public_html/area51/area51.php
@@ -1,6 +1,6 @@
 <?php 
 $regex = "/^(?![ ])(?!.*[ ]{2})((?:e|da|do|das|dos|de|d'|D'|la|las|el|los)\s*?|(?:[A-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð'][^\s]*\s*?)(?!.*[ ]$))+$/";
-$nome = "Alexandre Rodrigues";
+$nome = "Alexandre";
 $resultado = preg_match($regex, $nome);
 
 if ($resultado) { 
