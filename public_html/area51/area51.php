2761c79d54d3b4f3ad99504575196977beb08b55
Alexandre Rodrigues <novasystems.atendimento@hotmail.com>
2022-10-28 07:58:19 -0300

    update

--- public_html/area51/area51.php
+++ public_html/area51/area51.php
@@ -2,7 +2,7 @@
 $regex = "^(?:[\p{Lu}&&[\p{IsLatin}]])(?:(?:')?(?:[\p{Ll}&&[\p{IsLatin}]]))+(?:\-(?:[\p{Lu}&&[\p{IsLatin}]])(?:(?:')?(?:[\p{Ll}&&[\p{IsLatin}]]))+)*(?: (?:(?:e|y|de(?:(?: la| las| lo| los))?|do|dos|da|das|del|van|von|bin|le) )?(?:(?:(?:d'|D'|O'|Mc|Mac|al\-))?(?:[\p{Lu}&&[\p{IsLatin}]])(?:(?:')?(?:[\p{Ll}&&[\p{IsLatin}]]))+|(?:[\p{Lu}&&[\p{IsLatin}]])(?:(?:')?(?:[\p{Ll}&&[\p{IsLatin}]]))+(?:\-(?:[\p{Lu}&&[\p{IsLatin}]])(?:(?:')?(?:[\p{Ll}&&[\p{IsLatin}]]))+)*))+(?: (?:Jr\.|II|III|IV))?$";
 preg_match_all($regex, $nome, $resultado);
 
-if ($resultado) { 
+if ($resultado === true) { 
   echo "1"; 
 } else { 
   echo "2";
